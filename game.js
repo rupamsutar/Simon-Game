55e3daa02a2c925c9100d8aeaa74205ad1ab5f67
Rupam Sutar <rupamsutar281998@gmail.com>
2022-08-12 11:47:07 +0530

    Show the Sequence to the User with Animations and Sounds

--- game.js
+++ game.js
@@ -1,3 +1,19 @@
+var buttonColors = ["red", "blue", "green", "yellow" ];
+
+var gamePattern = [];
+
 function newSequence() {
   var randomNumber = Math.floor(4 * Math.random());
+
+  var randomChosenColor = buttonColors[randomNumber];
+
+  gamePattern.push(randomChosenColor);
+
+  $("#" + randomChosenColor).fadeOut(100).fadeIn(100).fadeOut(100).fadeIn(100);
+
+  var audio = new Audio("sounds/" + randomChosenColor + ".mp3");
+  audio.Play();
+
 }
+
+newSequence();
