554528d129711ac04e34eff7ab23a4769eadcd5c
Rupam Sutar <rupamsutar281998@gmail.com>
2022-08-14 10:47:59 +0530

    game-over fuction added

--- game.js
+++ game.js
@@ -1,77 +1,71 @@
-var buttonColours = ["red", "blue", "green", "yellow"];
-
+var buttonColors = ["red", "blue", "green", "yellow"];
 var gamePattern = [];
 var userClickedPattern = [];
-
 var started = false;
 var level = 0;
 
+
 $(document).keypress(function() {
   if (!started) {
-    $("#level-title").text("Level " + level);
+    $("h1").text("Level " + level);
     nextSequence();
     started = true;
   }
 });
 
-$(".btn").click(function() {
 
+$(".btn").click(function() {
   var userChosenColour = $(this).attr("id");
   userClickedPattern.push(userChosenColour);
-
   playSound(userChosenColour);
   animatePress(userChosenColour);
-
   checkAnswer(userClickedPattern.length - 1);
 });
 
 
 
 function checkAnswer(currentLevel) {
-
-
   if (gamePattern[currentLevel] === userClickedPattern[currentLevel]) {
-
     console.log("success");
-
-
     if (userClickedPattern.length === gamePattern.length) {
-
-
       setTimeout(function() {
         nextSequence();
       }, 1000);
-
     }
 
   } else {
-
     console.log("wrong");
 
-  }
+    playSound("wrong");
 
+    $("body").addClass("game-over");
+
+    setTimeout(function() {
+      $("body").removeClass("game-over");
+    }, 200);
+
+    $("h1").text("Game-Over ! Press any key to restart")
+  }
 }
 
+
 function nextSequence() {
-
-
   userClickedPattern = [];
-
   level++;
-  $("#level-title").text("Level " + level);
-
+  $("h1").text("Level " + level);
   var randomNumber = Math.floor(Math.random() * 4);
-  var randomChosenColour = buttonColours[randomNumber];
+  var randomChosenColour = buttonColors[randomNumber];
   gamePattern.push(randomChosenColour);
-
   $("#" + randomChosenColour).fadeIn(100).fadeOut(100).fadeIn(100);
   playSound(randomChosenColour);
 }
+
 
 function playSound(name) {
   var audio = new Audio("sounds/" + name + ".mp3");
   audio.play();
 }
+
 
 function animatePress(currentColor) {
   $("#" + currentColor).addClass("pressed");
